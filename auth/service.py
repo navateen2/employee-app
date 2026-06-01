971bdc1034170b52d259d683df3ab23514d9a326
navaneet <navateen2@gmail.com>
2026-06-01 17:59:27 +0530

    log+docker

--- auth/service.py
+++ auth/service.py
@@ -1,7 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from exceptions import *
 from repositories import employee_repo
+import logging
+
 from auth.utils import verify_password,create_access_token
+
+logger=logging.getLogger(__name__)
 
 async def login(db:AsyncSession, email:str, password:str)->str:
     employee= await employee_repo.get_by_email(db, email)
@@ -9,6 +13,7 @@ async def login(db:AsyncSession, email:str, password:str)->str:
         raise UnauthorizedException("Invalid email or password")
     if not verify_password(password, employee.password_hash):
         raise UnauthorizedException("Invalid email or password")
+    logger.info(f"User {email} logged in successfully")
     return create_access_token({
         "id":employee.id,
         "email":employee.email,
--- main.py
+++ main.py
@@ -19,6 +19,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s %(levelname)s %(name)s %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
+    filename="info.log"
 )
 
 @asynccontextmanager
